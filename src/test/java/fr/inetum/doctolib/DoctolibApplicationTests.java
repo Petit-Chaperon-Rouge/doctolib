9345ce2617a1c27fec73ce2a29dfb1fbcfc9c3f5
Axel Lecoeuche <axel.lecoeuche@sfr.fr.org>
2021-09-30 14:43:07 +0200

    init project

---
+++ src/test/java/fr/inetum/doctolib/DoctolibApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.inetum.doctolib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctolibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
